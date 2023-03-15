7340bf5cdad0bf4579976f93170458eaa3d42810
SeanXiaoby <sean_xiaoby@163.com>
2023-03-15 02:17:24 -0700

    Uploaded and updated Project#3 Backroads

---
+++ 3-backroads-app/src/components/data.js
@@ -0,0 +1,20 @@
+export const pageLinks = [
+  { id: 1, href: "#home", text: "home" },
+  { id: 2, href: "#about", text: "about" },
+  { id: 3, href: "#services", text: "services" },
+  { id: 4, href: "#tours", text: "tours" },
+];
+
+export const socialLinks = [
+  {
+    id: 1,
+    href: "https://www.instagram.com/sean_hahahaha/",
+    icon: "fab fa-instagram",
+  },
+  { id: 2, href: "https://twitter.com/seanshaw98", icon: "fab fa-twitter" },
+  {
+    id: 3,
+    href: "https://www.linkedin.com/in/boyang-xiao-40b644225/",
+    icon: "fab fa-linkedin-in",
+  },
+];
