1173cd1f104bc8e4f253fd605522c12c3af6bfc5
Boyang Xiao <sean_xiaoby@163.com>
2023-03-17 19:00:20 -0700

    Updated data for Project 4

--- 4-birthday-list/src/data.js
+++ 4-birthday-list/src/data.js
@@ -1,37 +1,37 @@
 export default [
   {
     id: 1,
-    name: 'Bertie Yates',
-    age: 29,
+    name: "Coco Wang",
+    age: 26,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
   },
   {
     id: 2,
-    name: 'Hester Hogan',
-    age: 32,
+    name: "Apuli Qiu",
+    age: 23,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg",
   },
   {
     id: 3,
-    name: 'Larry Little',
-    age: 36,
+    name: "Nicholas Chaos",
+    age: 34,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
   },
   {
     id: 4,
-    name: 'Sean Walsh',
-    age: 34,
+    name: "Sean Xiao",
+    age: 24,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
   },
   {
     id: 5,
-    name: 'Lola Gardner',
-    age: 29,
+    name: "Qiqiang Gao",
+    age: 56,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
   },
 ];
