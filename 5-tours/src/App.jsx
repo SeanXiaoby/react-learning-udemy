38c24902ccb21f446a2eec2608f006521ccd0d00
Boyang Xiao <sean_xiaoby@163.com>
2023-03-22 18:31:10 -0700

    Project 5 -tours finished

--- 5-tours/src/App.jsx
+++ 5-tours/src/App.jsx
@@ -1,6 +1,67 @@
-const url = 'https://course-api.com/react-tours-project';
+import { useEffect, useState } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+
+const url = "https://course-api.com/react-tours-project";
 
 const App = () => {
-  return <h2>Tours Starter</h2>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      if (response.status !== 200) {
+        throw new Error("Fetching data ERROR!");
+      }
+      const tempData = await response.json();
+      setTours(tempData);
+    } catch (err) {
+      console.log(err);
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <div className="title-underline" />
+          <button
+            className="btn"
+            style={{ marginTop: "2rem" }}
+            onClick={fetchData}
+          >
+            find more
+          </button>
+        </div>
+      </main>
+    );
+  }
+
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  );
 };
 export default App;
